7e3cbd401e97e50aa58ff3c73790d93c1e7b5661
Ricky.Lim <rickylim19@gmail.com>
2013-03-10 16:34:06 +0100

    change the order states and radius of the pie

--- get3D.R
+++ get3D.R
@@ -1,13 +1,15 @@
 # Create 3D pie chart for color chromatin in human
-
-color_states <- read.table("data/states_dom_hESC.txt.gz", header=T)
-head(color_states)
-coverage_color <- tapply(INDEX=color_states$tag, 
-                         X=color_states$end-color_states$start,
+H1color_states <- read.table("data/states_dom_hESC.txt.gz", header=T)
+head(H1color_states)
+coverage_color <- tapply(INDEX=H1color_states$tag, 
+                         X=H1color_states$end-H1color_states$start,
                          sum)
 percent_coverage_color <- coverage_color/sum(as.numeric(coverage_color))*100
 percent_coverage_color
+
+# reorder the color states
+percent_coverage_color <- percent_coverage_color[c(1,3,5,2,4)]
+percent_coverage_color
 library(plotrix)
-color_states =c("black", "deeppink2", "purple4", "red", "gold2")
-pie3D(percent_coverage_color, explode=0.1,
-      col = color_states)
+color_states =c("black", "purple4", "gold2", "deeppink2","red")
+pie3D(percent_coverage_color,col = color_states)
